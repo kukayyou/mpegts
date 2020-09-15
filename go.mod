a55dc6e49759bbde41e9be530c49d2b14a9279ef
kukayyou <838617725@qq.com>
2020-09-15 14:58:12 +0800

    add

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/kukayyou/mpegts
+
+go 1.14
+
+require (
+	github.com/deepch/old_bits v0.0.0-20160613011443-bacd5a88e1a1 // indirect
+	github.com/deepch/old_codec v0.0.0-20180218160520-de8dabf6b730
+)
---
+++ ts/track.go
@@ -0,0 +1,37 @@
+package ts
+
+import (
+	"bytes"
+	"github.com/deepch/old_codec/aacparser"
+)
+
+type Track struct {
+	SPS []byte
+	PPS []byte
+
+	Type int
+
+	pid       uint
+	PTS       int64
+	timeScale int64
+
+	mpeg4AudioConfig aacparser.MPEG4AudioConfig
+	buf              bytes.Buffer
+	payload          []byte
+	peshdr           *PESHeader
+	tshdr            TSHeader
+	spsHasWritten    bool
+	payloadReady     bool
+
+	demuxer   *Demuxer
+	mux       *Muxer
+	streamId  uint
+	tsw       *TSWriter
+	dataBuf   *iovec
+	cacheSize int
+}
+
+const (
+	H264 = 1
+	AAC  = 2
+)
